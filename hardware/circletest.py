387a83551d393fdaa9504d38dbf89f4018e4ba0f
Gurpreet Singh <gsingh@ca.ibm.com>
2017-04-01 22:07:42 -0400

    Finally have control over wheels

--- hardware/circletest.py
+++ hardware/circletest.py
@@ -1,21 +1,30 @@
 import Rpi.GPIO as GPIO
 import time
 
-def spin(pin, t):
+def go(pin):
     GPIO.output(pin, GPIO.HIGH)
-    time.sleep(t)
+    time.sleep(1)
     GPIO.output(pin, GPIO.LOW)
+
+
 
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.OUT) #left wheel forward
-GPIO.setup(12, GPIO.OUT) #left wheel back
-GPIO.setup(13, GPIO.OUT) #right wheen forward`
-GPIO.setup(15, GPIO.OUT) #right wheel backward
+GPIO.setup(13, GPIO.OUT) #left wheel back
+go(11)
+go(13)
 
-for e in range(0,10):
-    for i in [11,12,13,15]:
-        spin(i, 1);
+GPIO.cleanup()
+
+##We have to reset after each pair
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+
+go(16)
+go(18)
 
 GPIO.cleanup()
 
